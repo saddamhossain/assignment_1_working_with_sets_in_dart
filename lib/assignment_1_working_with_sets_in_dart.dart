f63745c7b6c0beafc75d5cb07aa9ffe7ffdf752e
saddamhossain <saddamhossain953@gmail.com>
2025-12-29 01:07:39 +0600

    Initial commit

---
+++ lib/assignment_1_working_with_sets_in_dart.dart
@@ -0,0 +1,18 @@
+void main()
+{
+ Set<int> SetA = {1, 2, 3, 4, 5};
+
+ Set<int> SetB = {4, 5, 6, 7, 8};
+
+ print("Elements of Set A:");
+ for(var item in SetA)
+ {
+   print(item);
+ }
+
+ print("Elements of Set B:");
+ for(var item in SetB)
+   {
+      print(item);
+   }
+}
